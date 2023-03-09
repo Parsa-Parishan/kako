bb23cee4b86e65ff77b73872325036bcded595af
Parsa Parishan <pparsaparishan@yahoo.com>
2023-03-09 14:21:08 -0500

    add landing page route

--- src/router/AnimatedRoutes.js
+++ src/router/AnimatedRoutes.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { AnimatePresence } from "framer-motion";
 import { Routes, Route, useLocation } from "react-router-dom";
-import Home from "../pages/Home"
+import Home from "../pages/Home";
+import Landing from "../pages/Landing";
 
 export default function AnimatedRoutes() {
   const location = useLocation();
   return (
     <AnimatePresence>
       <Routes location={location} key={location.pathname}>
-      <Route path="/kako" element={<Home />}>
-
+        <Route path="/kako" element={<Home />}>
+          <Route index element={<Landing />} />
         </Route>
       </Routes>
     </AnimatePresence>
