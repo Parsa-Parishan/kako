f7bd82dcd3009c7e4f0206803d4eedd5b9a28224
Parsa Parishan <pparsaparishan@yahoo.com>
2023-03-09 14:19:33 -0500

    add api and fetch data

---
+++ src/data/Data.js
@@ -0,0 +1,1 @@
+export const apiKey = "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";
---
+++ src/pages/Landing.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { apiKey } from "../data/Data";
+
+export default function Landing() {
+  const [value, setValue] = useState("");
+  const [answer, setAnswer] = useState("");
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const callApi = async () => {
+    const APIBody = {
+      model: "text-davinci-003",
+      prompt: value,
+      temperature: 0,
+      max_tokens: 60,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+    };
+
+    try {
+      const response = await fetch("https://api.openai.com/v1/completions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + apiKey,
+        },
+        body: JSON.stringify(APIBody),
+      });
+      if (response.ok) {
+        const jsonResponse = await response.json(response);
+        setAnswer(jsonResponse.choices[0].text);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <div>
+      <textarea
+        name=""
+        id=""
+        rows="1"
+        value={value}
+        onChange={handleChange}
+        placeholder="ask me a question"
+      />
+      <button onClick={callApi}>submit</button>
+      <div className="answer">{answer !== "" ? <div>{answer}</div> : null}</div>
+    </div>
+  );
+}
