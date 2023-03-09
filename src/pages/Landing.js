1a6523cfb69540c8316efec4160e36cb6d2104fd
Parsa Parishan <pparsaparishan@yahoo.com>
2023-03-09 18:04:38 -0500

    add landing page style

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,54 +1,23 @@
-import React, { useState } from "react";
-import { apiKey } from "../data/Data";
+import React from "react";
+
 
 export default function Landing() {
-  const [value, setValue] = useState("");
-  const [answer, setAnswer] = useState("");
-
-  const handleChange = (e) => {
-    setValue(e.target.value);
-  };
-
-  const callApi = async () => {
-    const APIBody = {
-      model: "text-davinci-003",
-      prompt: value,
-      temperature: 0,
-      max_tokens: 60,
-      top_p: 1.0,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0,
-    };
-
-    try {
-      const response = await fetch("https://api.openai.com/v1/completions", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + apiKey,
-        },
-        body: JSON.stringify(APIBody),
-      });
-      if (response.ok) {
-        const jsonResponse = await response.json(response);
-        setAnswer(jsonResponse.choices[0].text);
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  
   return (
-    <div>
-      <textarea
-        name=""
-        id=""
-        rows="1"
-        value={value}
-        onChange={handleChange}
-        placeholder="ask me a question"
-      />
-      <button onClick={callApi}>submit</button>
-      <div className="answer">{answer !== "" ? <div>{answer}</div> : null}</div>
+    <div className="landing">
+      <div className="intro">
+        <h2 className="first">Chat By Kako.ai:</h2>
+        <h2 className="second">Ask your Kako,</h2>
+        <h2 className="third">Whatever you need</h2>
+      </div>
+      <div className="chat">
+        <p>
+          <span>Find, Buy, Sell, or rent</span> houses with Kako.ai
+          <br />
+          Faster than any other Kako.ai
+        </p>
+      </div>
+      <div className="button">Try Chat with Kako.ai</div>
     </div>
   );
 }
