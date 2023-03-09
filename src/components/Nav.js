9c50157e122f6014efe34e637a0a6650b6d38a5b
Parsa Parishan <pparsaparishan@yahoo.com>
2023-03-09 17:26:36 -0500

    add nav style

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,32 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaBars } from "react-icons/fa";
 
 export default function Nav() {
+  const [slide, setSlide] = useState();
+
+  const handleClick = () => {
+    setSlide(!slide);
+  };
   return (
-    <div className='nav'>
-      <div className="log">
-        <h2>kako.ai</h2>
+    <div className="nav-wrapper">
+      <div className="nav">
+        <div className="logo">
+          <h2>
+            kako<span>.</span>ai
+          </h2>
+        </div>
+        <div className={`menu-bar ${slide && "active"}`} onClick={handleClick}>
+          <FaBars />
+        </div>
       </div>
-      <div className="options">
-        <div className="options-wrapper">
-          <option value="but">Buy</option>
-          <option value="sell">Sell</option>
-          <option value="rent">Rent</option>
-          <option value="mortgage">Mortgage</option>
-          <option value="pricing">Pricing</option>
-          <div className="resources">Resources</div>
-        </div >
+      <div className={`options ${slide && "slide-down"}`}>
+        <div value="but">Buy</div>
+        <div value="sell">Sell</div>
+        <div value="rent">Rent</div>
+        <div value="mortgage">Mortgage</div>
+        <div value="pricing">Pricing</div>
+        <div className="resources">Resources</div>
       </div>
     </div>
-  )
+  );
 }
